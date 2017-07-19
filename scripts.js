e341ff21f879b47f42bf77dd67b8f8e61530582e
unknown <rosiek.kuba@gmail.com>
2017-07-19 13:03:46 +0100

    pole trojkata

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+
+var a,
+    h,
+    triangleArea;
+a=2;
+h=10;	
+
+triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
